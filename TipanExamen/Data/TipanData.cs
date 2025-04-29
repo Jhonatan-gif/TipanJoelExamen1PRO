9fe11beb5dccc0cb7df3bff4f5e6c90b9b4cedf4
Jhonatan Tipan <jhonatan.tipan@udla.edu.ec>
2025-04-29 15:49:11 -0500

    Creacion de ClienteController y views tipo cliente

---
+++ TipanExamen/Data/TipanData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TipanExamen.Models;
+
+    public class TipanData : DbContext
+    {
+        public TipanData (DbContextOptions<TipanData> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TipanExamen.Models.Cliente> Cliente { get; set; } = default!;
+    }
--- TipanExamen/Program.cs
+++ TipanExamen/Program.cs
@@ -1,4 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TipanData>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TipanData") ?? throw new InvalidOperationException("Connection string 'TipanData' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
